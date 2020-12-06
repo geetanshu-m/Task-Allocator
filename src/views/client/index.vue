cd69665736afd029f2eca330c36b2ba4525666e3
proman24 <mathur.geetanshu887@gmail.com>
2020-12-06 18:50:18 +0530

    task view screen

--- src/views/client/index.vue
+++ src/views/client/index.vue
@@ -1,15 +1,42 @@
 <template>
   <div id="client-main">
-      Hello from client
+    <h1>All Task</h1>
+    <div id="task-list">
+      <div class="task" v-for="(task, index) in taskList" :key="index">
+        {{task.content}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'task-list',
+  data() {
+    return {
+      taskList: [
+        {
+          content: 'task 1',
+        },
+        {
+          content: 'task 2',
+        },
+        {
+          content: 'task 3',
+        },
+        {
+          content: 'task 4',
+        },
+      ],
+    };
+  },
 
 };
 </script>
 
-<style>
-
+<style lang="scss">
+#task-list{
+  display:flex;
+  flex-direction: column;
+}
 </style>
