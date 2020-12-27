f4b49c15e28c59d4440d89e23b6ec5d72b77bf25
proman24 <mathur.geetanshu887@gmail.com>
2020-12-27 22:28:45 +0530

    adding required modals

--- api/api/tasks/models.py
+++ api/api/tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class company(models.Model):
+    company_name = models.CharField(max_length=120)
+    company_employees = models.ManyToManyField(User)
+    class Meta:
+        db_table = 'company'
+
+class user_tasks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_title = models.CharField(max_length=120)
+    task_detail = models.TextField()
+    completed = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(default=timezone.now())
--- api/api/tasks/views.py
+++ api/api/tasks/views.py
@@ -1,3 +1,14 @@
+'''
+Following API's are needed
+
+- Company Creation flow, company_admin creates account, create company, add employees,
+- Validate users
+- User task [CRUD]
+    - user can add a task
+    - update a task [Mark as complete]
+    - Get a list of his tasks
+'''
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
